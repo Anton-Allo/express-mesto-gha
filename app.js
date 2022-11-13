c4a7ae63504a8f1428f75ed9c23739af7bd9207a
Anton Allo <qqqqton1@yandex.ru>
2022-11-13 23:13:09 +0300

    Fix handle errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const { BAD_REQUEST, SERVER_ERROR } = require("./utils/errors");
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,8 +18,25 @@ app.use((req, res, next) => {
 
 app.use(routes);
 
-app.use("*", () => {
-  throw new NotFoundError("Не найдено");
+app.use("*", (err, req, res, next) => {
+  if (err.name === "CastError") {
+    res.status(BAD_REQUEST).send({ message: "Переданы некорректные данные" });
+  } else if (err.name === "ValidationError") {
+    res.status(BAD_REQUEST).send({
+      message: `${Object.values(err.errors)
+        .map((error) => error.message)
+        .join(", ")}`,
+    });
+  } else {
+    const { statusCode = SERVER_ERROR, message } = err;
+    res
+      .status(statusCode)
+      .send({
+        message:
+          statusCode === SERVER_ERROR ? "На сервере произошла ошибка" : message,
+      });
+  }
+  next();
 });
 
 mongoose.connect("mongodb://127.0.0.1:27017/mydb");
