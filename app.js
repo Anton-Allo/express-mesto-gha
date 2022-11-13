dd862be3b7d0ca3abfd0d1f2cf9356aa9b4f0077
Anton Allo <qqqqton1@yandex.ru>
2022-11-14 00:01:55 +0300

    Fix#8

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use((req, res, next) => {
 
 app.use(routes);
 
-app.use("*", (req, res, next) => {
-  res.status(404).send({ message: "сервер не найден" });
+app.use((req, res, next) => {
+  res.status(NOT_FOUND).send({ message: "сервер не найден" });
   next();
 });
 
