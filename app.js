b3872c7653df031b54b7c611649dedc8efaf64ce
Anton Allo <qqqqton1@yandex.ru>
2022-11-14 00:45:14 +0300

    Fix#13

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const http2 = require("http2");
 
 const { PORT = 3000 } = process.env;
 const app = express();
