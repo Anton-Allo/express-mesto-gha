65125b9f2e6e29d2bc4de4f3ec40fc28fed7d3ec
Anton Allo <qqqqton1@yandex.ru>
2022-11-14 00:25:59 +0300

    Fix#10

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
-const { NOT_FOUND } = require("./utils/errors");
+import { constants } from "http2";
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -18,9 +18,10 @@ app.use((req, res, next) => {
 
 app.use(routes);
 
-app.all("/*", (req, res, next) => {
-  res.status(NOT_FOUND).send({ message: "сервер не найден" });
-  next();
+app.all("/*", (req, res) => {
+  res
+    .status(constants.HTTP_STATUS_NOT_FOUND)
+    .send({ message: "страницы не существует" });
 });
 
 mongoose.connect("mongodb://127.0.0.1:27017/mydb");
