06c1a9821d5c78f8099efae7492d11fcf80e5ff4
Anton Allo <qqqqton1@yandex.ru>
2022-11-13 23:33:55 +0300

    Fix#3 handle errors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
-const { BAD_REQUEST, SERVER_ERROR } = require("./utils/errors");
+const { NOT_FOUND } = require("./utils/errors");
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -18,24 +18,9 @@ app.use((req, res, next) => {
 
 app.use(routes);
 
-app.use((err, req, res, next) => {
-  if (err.name === "CastError") {
-    res.status(BAD_REQUEST).send({ message: "Переданы некорректные данные" });
-  } else if (err.name === "ValidationError") {
-    res.status(BAD_REQUEST).send({
-      message: `${Object.values(err.errors)
-        .map((error) => error.message)
-        .join(", ")}`,
-    });
-  } else {
-    const { statusCode = SERVER_ERROR, message } = err;
-    res.status(statusCode).send({
-      message:
-        statusCode === SERVER_ERROR ? "На сервере произошла ошибка" : message,
-    });
-  }
-  next();
-});
+app.use("*", (req, res) =>
+  res.status(NOT_FOUND).send({ message: "несуществующая страница" })
+);
 
 mongoose.connect("mongodb://127.0.0.1:27017/mydb");
 
