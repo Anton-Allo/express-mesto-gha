e428ba9c8bf0d57e5c626a2de6a45805fb0dec29
Anton Allo <qqqqton1@yandex.ru>
2022-11-14 22:54:08 +0300

    Fix#19

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.use((req, res, next) => {
 
 app.use(routes);
 
-app.all("/*", (req, res) => {
+app.all("/*", (req, res, next) => {
   res.status(404).send({ message: "страницы не существует" });
+  next();
 });
 
 mongoose.connect("mongodb://127.0.0.1:27017/mydb");
